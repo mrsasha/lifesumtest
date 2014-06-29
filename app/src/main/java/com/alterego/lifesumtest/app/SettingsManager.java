5c314d4451423b6684c5fc57314081fd145660e1
Sasa Sekulic <ja@sasasekulic.info>
2014-06-29 03:37:19 +0200

    added Dao methods to SettingsMgr

--- app/src/main/java/com/alterego/lifesumtest/app/SettingsManager.java
+++ app/src/main/java/com/alterego/lifesumtest/app/SettingsManager.java
@@ -2,17 +2,28 @@ package com.alterego.lifesumtest.app;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.alterego.advancedandroidlogger.implementations.NullAndroidLogger;
 import com.alterego.advancedandroidlogger.interfaces.IAndroidLogger;
+import com.alterego.lifesumtest.DaoMaster;
+import com.alterego.lifesumtest.DaoSession;
+import com.alterego.lifesumtest.LifesumItem;
+import com.alterego.lifesumtest.LifesumItemDao;
+import com.alterego.lifesumtest.LifesumSearchDaoEntryDao;
 import com.alterego.lifesumtest.app.api.LifesumApiManager;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import rx.Observable;
+import rx.Subscriber;
 
 @Accessors(prefix="m")
 public class SettingsManager {
@@ -24,13 +35,14 @@ public class SettingsManager {
     @Getter @Setter Application mParentApplication;
     @Getter @Setter IAndroidLogger mLogger = NullAndroidLogger.instance;
     @Getter private Gson mGson = new GsonBuilder().create();
-    //@Getter private Gson mGson = new GsonBuilder().registerTypeAdapter(DateTime.class, dateSerializer).create();
+    @Getter private LifesumItemDao mLifesumItemDao;
+    @Getter private LifesumSearchDaoEntryDao mLifesumSearchDaoEntryDao;
 
     public SettingsManager(Application app, IAndroidLogger logger, ImageLoaderConfiguration imageLoaderConfig) {
         setLogger(logger);
         setParentApplication(app);
         mImageLoaderConfiguration = imageLoaderConfig;
-        //TODO setupDatabase(app);
+        setupDatabase(app);
         mLifesumApiManager = new LifesumApiManager(this);
     }
 
@@ -38,6 +50,64 @@ public class SettingsManager {
         mParentActivity = act;
     }
 
+    private void setupDatabase(Context ctx) {
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(ctx, "lifesumtest-db", null);
+        SQLiteDatabase database = helper.getWritableDatabase();
+        DaoMaster daoMaster = new DaoMaster(database);
+        DaoSession daoSession = daoMaster.newSession();
+        mLifesumItemDao = daoSession.getLifesumItemDao();
+        mLifesumSearchDaoEntryDao = daoSession.getLifesumSearchDaoEntryDao();
+    }
 
+    /********************* DAO MANAGEMENT *******************************/
+
+    public Observable<List<LifesumItem>> loadLifesumItems() {
+        return Observable.create(new Observable.OnSubscribe<List<LifesumItem>>() {
+            @Override
+            public void call(Subscriber<? super List<LifesumItem>> subscriber) {
+                try {
+                    List<LifesumItem> items = mLifesumItemDao.loadAll();
+                    mLogger.info("SettingsManager loadLifesumItems size = " + items.size());
+                    subscriber.onNext(items);
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
+
+    public Observable<Boolean> storeLifesumItems(final List<LifesumItem> items) {
+        return Observable.create(new Observable.OnSubscribe<Boolean>() {
+            @Override
+            public void call(Subscriber<? super Boolean> subscriber) {
+                try {
+                    for (LifesumItem item : items) {
+                        mLifesumItemDao.insert(item);
+                    }
+                    mLogger.info("SettingsManager storeLifesumItems successful, new items added size = " + items.size());
+                    subscriber.onNext(true);
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
+
+    public Observable<Boolean> deleteLifesumItems(final List<LifesumItem> items) {
+        return Observable.create(new Observable.OnSubscribe<Boolean>() {
+            @Override
+            public void call(Subscriber<? super Boolean> subscriber) {
+                try {
+                    for (LifesumItem item : items) {
+                        mLifesumItemDao.delete(item);
+                    }
+                    mLogger.info("SettingsManager deleteLifesumItems successful, removed items size = " + items.size());
+                    subscriber.onNext(true);
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
 
 }
