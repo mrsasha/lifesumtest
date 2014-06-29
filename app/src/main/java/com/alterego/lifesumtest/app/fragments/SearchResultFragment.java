241207e8e6fb8a045ad8258493ee84863a366e4e
Sasa Sekulic <ja@sasasekulic.info>
2014-06-29 04:31:23 +0200

    added item saving to SearchResultFragment

--- app/src/main/java/com/alterego/lifesumtest/app/fragments/SearchResultFragment.java
+++ app/src/main/java/com/alterego/lifesumtest/app/fragments/SearchResultFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
+import android.widget.Toast;
 
 import com.alterego.lifesumtest.LifesumItem;
 import com.alterego.lifesumtest.app.MainApplication;
@@ -25,9 +26,11 @@ import com.alterego.lifesumtest.app.data.LifesumResponse;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchResultFragment extends Fragment
-        //implements AbsListView.OnItemClickListener, AdapterView.OnItemLongClickListener
-{
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class SearchResultFragment extends Fragment {
 
     private static final String LIFESUM_SEARCH_RESULT = "lifesum_search_result";
 
@@ -76,8 +79,6 @@ public class SearchResultFragment extends Fragment
         mListView = (AbsListView) view.findViewById(android.R.id.list);
         ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
 
-        //mListView.setOnItemClickListener(this);
-        //mListView.setOnItemLongClickListener(this);
         mListView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
         mListView.setMultiChoiceModeListener(mListViewMultiChoiceModeListener);
 
@@ -100,63 +101,6 @@ public class SearchResultFragment extends Fragment
         super.onDetach();
         mListener = null;
     }
-
-
-//    @Override
-//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//        if (null != mListener) {
-//            //TODO open details fragment
-//            // Notify the active callbacks interface (the activity, if the
-//            // fragment is attached to one) that an item has been selected.
-//            //mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
-//        }
-//    }
-
-//    @Override
-//    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-//        //TODO select item to save!
-//        if (mActionMode != null) {
-//            return false;
-//        }
-//
-//        mActionMode = getActivity().startActionMode(mActionModeCallback);
-//        view.setSelected(true);
-//        return true;
-//    }
-
-//    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
-//
-//        @Override
-//        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-//            // Inflate a menu resource providing context menu items
-//            MenuInflater inflater = mode.getMenuInflater();
-//            inflater.inflate(R.menu.save, menu);
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-//            return false; // Return false if nothing is done
-//        }
-//
-//        @Override
-//        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.action_save:
-//                    saveSelectedItems();
-//                    mode.finish(); // Action picked, so close the CAB
-//                    return true;
-//                default:
-//                    return false;
-//            }
-//        }
-//
-//        // Called when the user exits the action mode
-//        @Override
-//        public void onDestroyActionMode(ActionMode mode) {
-//            mActionMode = null;
-//        }
-//    };
 
     private AbsListView.MultiChoiceModeListener mListViewMultiChoiceModeListener = new AbsListView.MultiChoiceModeListener() {
 
@@ -216,6 +160,22 @@ public class SearchResultFragment extends Fragment
                 selectedItems.add(mAdapter.getItem(position));
         }
 
+        mSettingsManager.storeLifesumItems(selectedItems).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(new Observer<Boolean>() {
+            @Override
+            public void onCompleted() {
+                //do nothing
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(mSettingsManager.getParentActivity(), "Problem saving items!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(Boolean aBoolean) {
+                Toast.makeText(mSettingsManager.getParentActivity(), "Saving items successful!", Toast.LENGTH_SHORT).show();
+            }
+        });
         mSettingsManager.getLogger().info("SearchResultFragment saveSelectedItems size = " + selectedItems.size());
 
     }
